6c4b74a4fad5678fd439dc6360317f21ee75d3b0
Salahutdinov Dmitry <dsalahutdinov@gmail.com>
2017-02-26 09:02:23 +0500

    f

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type webhook struct {
+	Action     string
+	Repository struct {
+		ID       string
+		FullName string
+	}
+}
+
+func handleWebhook(w http.ResponseWriter, r *http.Request) {
+	webhookData := make(map[string]interface{})
+	err := json.NewDecoder(r.Body).Decode(&webhookData)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	fmt.Println("Got webhook payload: ")
+	for k, v := range webhookData {
+		fmt.Printf("%s : %v\n", k, v)
+	}
+	fmt.Printf("headers: %v\n", r.Header)
+}
+
+func main() {
+	log.Println("server started")
+	http.HandleFunc("/webhook", handleWebhook)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
